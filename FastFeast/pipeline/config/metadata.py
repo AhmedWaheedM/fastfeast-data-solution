69b6cdb844501782b00d47ec42a06c55ef42872a
Mariam Taha <mariamtaha2224@gmail.com>
2026-03-27 03:05:28 +0200

    feat: add python script for metadata to import it later

---
+++ FastFeast/pipeline/config/metadata.py
@@ -0,0 +1,47 @@
+from dataclasses import dataclass
+import yaml
+from typing import List
+import os
+from pathlib import Path
+from dacite import from_dict
+from typing import Any, Dict, Optional
+
+
+
+@dataclass
+class Column:
+    name: str
+    type: str
+    pk: bool = False
+    nullable: bool = True
+
+    # optional extra metadata
+    fk: Optional[Dict[str, Any]] = None
+    expected_values: Optional[List[str]] = None
+    range: Optional[Dict[str, float]] = None
+    pii: Optional[bool] = None
+    format: Optional[str] = None
+
+@dataclass
+class FileMeta:
+    file_name: str
+    columns: List[Column]
+
+@dataclass
+class Settings:
+    batch: List[FileMeta]
+
+def load(path: str) -> Settings:
+    with open(path, "r") as f:
+        data = yaml.safe_load(f)
+
+    return from_dict(
+        data_class=Settings,
+        data=data,
+    )
+
+
+#Call load function to return data from yaml file
+#import this directly in .py script
+yaml_path = Path(os.getenv("FILES_METADATA_YAML", Path(__file__).parent / "files_metadata.yaml"))
+metadata_settings = load(yaml_path)
