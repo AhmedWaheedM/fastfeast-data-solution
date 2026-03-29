5c5700c6cf5420d123f6194f6a82b673f11cfc61
SalmaAlgayar <salmaalgayar47@gmail.com>
2026-03-29 19:23:48 +0200

    fix: importing missed libraries

--- FastFeast/pipeline/ingestion/batch_file_tracker.py
+++ FastFeast/pipeline/ingestion/batch_file_tracker.py
@@ -1,10 +1,7 @@
+from utilities.db_utils import get_connection
 
-
-
-
-
-
-def get_last_state(conn, file_name):
+def get_last_state(file_name):
+    conn = get_connection()
     row = conn.execute(
         "SELECT last_hash, last_checkpoint FROM BATCH_FILE_TRACKING WHERE file_name = ?",
         (file_name,)
@@ -12,7 +9,8 @@ def get_last_state(conn, file_name):
     return row if row else (None, None)
 
 
-def update_state(conn, file_name, file_hash, checkpoint):
+def update_state(file_name, file_hash, checkpoint):
+    conn = get_connection()
     conn.execute("""
         INSERT OR REPLACE INTO BATCH_FILE_TRACKING (file_name, last_hash, last_checkpoint)
         VALUES (?, ?, ?)
