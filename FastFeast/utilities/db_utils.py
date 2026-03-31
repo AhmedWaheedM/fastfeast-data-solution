c2d7ad05522b1762479681d5f84370e14e035e7d
Mariam Taha <mariamtaha2224@gmail.com>
2026-03-31 04:38:49 +0200

    refactor: comment out unused _run_ddl function in get_connection

--- FastFeast/utilities/db_utils.py
+++ FastFeast/utilities/db_utils.py
@@ -11,20 +11,20 @@ def get_connection():
         db_path = os.path.join(cfg.paths.output_dir, cfg.database.file) # merge output dir and db file name to get full path to db
         os.makedirs(cfg.paths.output_dir, exist_ok=True)
         _connection = duckdb.connect(db_path)
-        _run_ddl(_connection)
+        #_run_ddl(_connection)
     return _connection
 
-def _run_ddl(conn):
-    """
-    Run All DDL files to ensure tables exist 
-    """
-    import os 
-    ddl_dirs = ['FastFeast/dwh/bronze', 'FastFeast/dwh/silver', 'FastFeast/dwh/gold']
-    for directory in ddl_dirs: 
-        if not os.path.exists(directory):
-            continue
-        for filename in sorted(os.listdir(directory)):
-            if filename.endswith('.sql'):
-                with open(os.path.join(directory, filename), 'r') as f:
-                    sql = f.read()
-                    conn.execute(sql)
+# def _run_ddl(conn):
+#     """
+#     Run All DDL files to ensure tables exist 
+#     """
+#     import os 
+#     ddl_dirs = ['FastFeast/dwh/bronze', 'FastFeast/dwh/silver', 'FastFeast/dwh/gold']
+#     for directory in ddl_dirs: 
+#         if not os.path.exists(directory):
+#             continue
+#         for filename in sorted(os.listdir(directory)):
+#             if filename.endswith('.sql'):
+#                 with open(os.path.join(directory, filename), 'r') as f:
+#                     sql = f.read()
+#                     conn.execute(sql)
