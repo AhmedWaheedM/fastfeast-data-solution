c19ee983426e95b2fe9f84bf38e761b9592f9ac0
Mariam Taha <mariamtaha2224@gmail.com>
2026-04-05 21:43:32 +0200

    refactor: rename validation utility functions for clarity

--- FastFeast/pipeline/ingestion/listen_to_folder.py
+++ FastFeast/pipeline/ingestion/listen_to_folder.py
@@ -10,7 +10,7 @@ from FastFeast.pipeline.config.config import get_config
 from FastFeast.pipeline.bridge.pyarrow_table import load_file
 from FastFeast.utilities.metadata_cash import compare_columns
 from FastFeast.pipeline.validation.schema_validation import validate_table
-from FastFeast.utilities.validation_utils import expected_types, not_null_column, column_format, range, map_format_to_pattern, map_type_to_pyarrow, map_type_to_pattern
+from FastFeast.utilities.validation_utils import expected_types, get_nullable_columns, column_format, get_column_range, map_format_to_pattern, map_type_to_pyarrow, map_type_to_pattern
 
 
 ####################################################
@@ -90,10 +90,10 @@ def process_single_file():
 
         expected_types_str = expected_types(file, type)
         expected_types_pa = map_type_to_pyarrow(expected_types_str)
-        not_null = not_null_column(file, type)
+        not_null = get_nullable_columns(file, type)
         expected_formats_str = column_format(file, type)
         expected_formats = map_format_to_pattern(expected_formats_str)
-        column_range = range(file, type)
+        column_range = get_column_range(file, type)
         expected_pattern = map_type_to_pattern(expected_types_str)
 
         status_list, error_lists, retry_list = validate_table(pa_table, expected_types_pa, expected_pattern, not_null, expected_formats, column_range)
@@ -101,5 +101,3 @@ def process_single_file():
         return status_list, error_lists, retry_list
     
 ####################################################
-
-
--- FastFeast/utilities/validation_utils.py
+++ FastFeast/utilities/validation_utils.py
@@ -26,7 +26,7 @@ def expected_types(file_name, type):
 
 ############################################
 
-def not_null_column(file_name, type):
+def get_nullable_columns(file_name, type):
     source = metadata_settings.batch if type == 'batch' else metadata_settings.stream
     notnull = []
 
@@ -43,31 +43,31 @@ def not_null_column(file_name, type):
 
 def column_format(file_name, type):
     source = metadata_settings.batch if type == 'batch' else metadata_settings.stream
-    format = {}
+    columns_format = {}
 
     for file_meta in source:
         if file_meta.file_name == file_name:
             for column in file_meta.columns:
                 if column.format:
-                    format[column.name] = column.format
+                    columns_format[column.name] = column.format
 
-            return format  
-    return format
+            return columns_format  
+    return columns_format
 
 ############################################
 
-def range(file_name, type):
+def get_column_range(file_name, type):
     source = metadata_settings.batch if type == 'batch' else metadata_settings.stream
-    range = {}
+    column_range = {}
 
     for file_meta in source:
         if file_meta.file_name == file_name:
             for column in file_meta.columns:
                 if column.range:
-                    range[column.name] = column.range
+                    column_range[column.name] = column.range
 
-            return range  
-    return range
+            return column_range  
+    return column_range
 
 ############################################
 
